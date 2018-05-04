e95178df1fe21796650d7f3db1c19b153f310b1e
bingou1 <740100506@qq.com>
2018-05-04 17:56:08 +0800

    version 1.0.1

---
+++ .history/extension_20180427174021.js
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "FesHelper" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.sayHello",
+    function() {
+      // The code you place here will be executed every time your command is executed
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return; // 没有打开的文件
+      }
+
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
+
+      // 向用户显示一个消息框
+      vscode.window.showInformationMessage(
+        "Selected characters: " + text.length
+      );
+      // Display a message box to the user
+    //   vscode.window.showInformationMessage("Hello World!1111");
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# FesHelper
-Fes Api snippets
+# FesHelper README
+![logo](./src/images/logo.jpg)
+These snippets were built to supercharge my workflow in the most seamless manner possible.
+# Installation
+click the extensions button (lowest square icon in the editor), and type in FesHelper, select the one by sdras
+
+# Snippets
+Temlate: Components start with wb, you can try yourself by typing wb
+Script: Fes instance api start with fes$, you can try yourself by typing fes$
